3d60a54638a8b54e6e89cb1810df2cc6296336b9
emmanuelidiaghe <emmanuelidiaghe@gmail.com>
2022-01-16 01:20:07 +0100

    Pushed initial commit

---
+++ MovieApp/Data/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace MovieApp.Data
+{
+    public class Movie
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("overview")]
+        public string Overview { get; set; }
+
+        [JsonProperty("release_date")]
+        public string Release_Date { get; set; }
+
+        [JsonProperty("vote_average")]
+        public string Vote_Average { get; set; }
+
+        [JsonProperty("vote_count")]
+        public string Vote_Count { get; set; }
+
+        [JsonProperty("poster_path")]
+        public string Poster_Path { get; set; }
+    }
+}
---
+++ MovieApp/Data/Response.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace MovieApp.Data
+{
+    public class Response
+    {
+        [JsonProperty("page")]
+        public int Page { get; set; }
+
+
+        [JsonProperty("results")]
+        public List<Movie> Movies { get; set; }
+    }
+}
